6930e822e94e8190b714a310c89fe540018c754d
whlong1 <whunterlong@gmail.com>
2022-06-30 09:01:29 -0400

    clean up index.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import App from './App/App'
 import React from 'react'
+import App from './App/App'
 import './styles/index.css'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
